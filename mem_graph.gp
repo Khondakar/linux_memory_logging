b81f5b980fede7ff636701957b9ed6fc1d69f58a
Jackson Wiegman <jackson@providiusdesign.com>
2016-05-12 19:30:38 -0400

    Tweak graph and add experimental files 	modified:   mem_graph.gp 	modified:   mem_graph.sh 	mem_graph.gp_old 	mem_graph.sh.exp 	mem_log_all.sh

--- mem_graph.gp
+++ mem_graph.gp
@@ -1,4 +1,7 @@
+set terminal wxt size 1920,1080
 set key outside;
-set key right;
+set key right center;
+set key width 1;
+set key samplen 2 font ",8";
 list = system('ls $filepath/*.log')
-plot for [file in list] file title system('basename '.file)
+plot for [file in list] file title system('basename '.file.' | cut -f 1 -d "."')
--- mem_graph.sh
+++ mem_graph.sh
@@ -3,16 +3,28 @@
 
 LOG_FILE=$1
 LOG_TEMP_DIR=/tmp/$1_plot
+LOG_EXT=log
 
 rm -r $LOG_TEMP_DIR
 mkdir $LOG_TEMP_DIR
 
+num_pids=$(cat $LOG_FILE | awk '{print $3}' | sort | uniq | wc -w)
+
+#Get all unique pid's from the file, then loop through and output their mem use
+echo Parsing File...
 for x in $(cat $LOG_FILE | awk '{print $3}' | sort | uniq);
 do 
     cat $LOG_FILE | \
-    awk -v var="$x" '{if ($3 == var) print $5}' > $LOG_TEMP_DIR/$x.log
+    awk -v var="$x" '{if ($3 == var) print $5}' > $LOG_TEMP_DIR/$x.$LOG_EXT
+    echo -ne $num_pids 'pids left\r'
+    num_pids=$((num_pids-1))
 done
 
+#ps will track lots of threads, so delete entries that are small
+echo Deleting Files...
+find $LOG_TEMP_DIR -name "*.$LOG_EXT" -size -100c -delete
+
+echo Generating Graph...
 export filepath=$LOG_TEMP_DIR
 gnuplot --persist  mem_graph.gp
-rm -r $LOG_TEMP_DIR
+#rm -r $LOG_TEMP_DIR
